8b7ac31beed23bd8116c68c33f94e68f6ca82ae6
gagandua078 <39922405+gagandua078@users.noreply.github.com>
2021-11-11 10:21:31 +1100

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,41 @@
+
+function searchShow(query) {
+    const url = `https://api.tvmaze.com/search/shows?q=${query}`;
+    fetch(url)
+    .then(response => response.json())
+    .then((jsonData) => {
+        const results = jsonData.map(element => element.show.name);
+        renderResults(results);
+        document.getElementById("errorMessage").innerHTML = "";
+    })
+    .catch((error) => {
+        document.getElementById("errorMessage").innerHTML = error;
+        renderResults([]);
+    }); 
+}
+
+function renderResults(results) {
+    const list = document.getElementById("resultsList");
+    list.innerHTML = "";
+    results.forEach(result => {
+        const element = document.createElement("li");
+        element.innerText = result;
+        list.appendChild(element);
+    });
+}
+
+let searchTimeoutToken = 0;
+window.onload = () => {
+    const searchFieldElement = document.getElementById("ShowSearchFields");
+    searchFieldElement.onkeyup = (event) => {
+
+        clearTimeout(searchTimeoutToken);
+        if(searchFieldElement.value.trim().length === 0) {
+            return;
+        }
+
+        searchTimeoutToken = setTimeout(() => {
+            searchShow(searchFieldElement.value);
+        }, 250);
+    };
+}
